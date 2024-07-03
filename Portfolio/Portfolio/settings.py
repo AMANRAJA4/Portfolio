b4f1f1dbf8427720e661478f18730259e9e9cdce
Aman Raja <86131513+AMANRAJA4@users.noreply.github.com>
2024-07-03 05:49:17 +0530

    build file

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hn96__ums^a%w1=6mu=hg)k28sy$c6_mhmilyr@&c-b2@5#r(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','.vercel.app','amanraja.online']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','amanraja.online','localhost', '127.0.0.1',]
 
 
 # Application definition
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'Portfolio.wsgi.app'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'sKYXkMekOGSqSjVTcQpflyEHQPtqFIFm',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '53228',
     }
 }
 
@@ -117,8 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
